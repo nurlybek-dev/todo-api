5580a5f2bf8280d2652bec0ec0901d323c1edf82
Orynbekov Nurlybek <nurlybek.dev@gmail.com>
2021-10-02 17:45:01 +0600

    Модель задач, сериализатор и набор представлений

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    priority = models.IntegerField(blank=True, null=True)
+    deadline = models.DateTimeField(blank=True, null=True)
--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,9 @@
 from rest_framework import routers
 
+from .views import TaskViewSet
+
+
 router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet)
 
 urlpatterns = router.urls
